01bb5917c36aa8f5db1f48369087e642f982ae77
Adrian Gumiński <guminskiadrian34@gmail.com>
2026-04-01 15:22:14 +0200

    initial commit

---
+++ football.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
+
+API_KEY = os.getenv("FOOTBALL_API_KEY")
+BASE_URL = "https://api.football-data.org/v4"
+headers = {"X-Auth-Token" : API_KEY}
+
+response = requests.get(f"{BASE_URL}/competitions/PL", headers = headers)
+
+def get_standings(league_code):
+    response = requests.get(f'{BASE_URL}/competitions/{league_code}/standings', headers=headers)
+    data = response.json()
+
+    standings = data["standings"][0]["table"]
+
+    print(f'\n{'Pos':<5}{'Team':<30}{'P':<5}{'W':<5}{'D':<5}{'L':<5}{'GF':<5}{'GA':<5}{'GD':<5}{'Pts':<5}')
+    print('-' * 74)
+
+    for team in standings:
+        print(f'{team["position"]:<5}{team["team"]["name"]:<30}{team["playedGames"]:<5}{team["won"]:<5}{team["draw"]:<5}{team["lost"]:<5}{team["goalsFor"]:<5}{team["goalsAgainst"]:<5}{team["goalDifference"]:<5}{team["points"]:<5}')
+
+get_standings("PL")
